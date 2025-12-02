22566c6275893644ef8f67901c9ce7d6fa40f749
Nikita V <nikita.postpunk@gmail.com>
2025-12-02 23:01:13 +0100

    1.1: Parse input

--- 2025/01/1.rs
+++ 2025/01/1.rs
@@ -1,12 +1,13 @@
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
-#[allow(dead_code)]
+#[derive(Debug)]
 enum Direction {
     Left,
     Right,
 }
 
+#[derive(Debug)]
 #[allow(dead_code)]
 struct Rotation {
     dir: Direction,
@@ -15,21 +16,38 @@ struct Rotation {
 
 // dial safe clicks range is 0..90, 50 is initial
 fn main() {
-    println!("hello");
-    let _ = load_rotations("in.txt");
+    let res = load_rotations("in.txt");
+    match res {
+        Ok(rotations) => {
+            println!("{:#?}", rotations);
+        }
+        Err(e) => eprintln!("Unexpected error: {}", e),
+    }    
 }
 
-// fn load_rotations(filepath: String) -> io::Result<Vec<Rotation>> {
-fn load_rotations(filepath: &str) -> io::Result<()> {
+fn load_rotations(filepath: &str) -> io::Result<Vec<Rotation>> {
     let file = File::open(filepath)?;
     let mut reader = BufReader::new(file);
 
+    let mut rotations: Vec<Rotation> = Vec::new();
     let mut buf = String::new();
 
     while reader.read_line(&mut buf)? != 0 {
-        println!("{}", buf);
+        let dir = match buf.chars().next() {
+            Some('L') => Direction::Left,
+            Some('R') => Direction::Right,
+            _ => unreachable!("Unexpected direction"),
+        };
+
+        let clicks = buf[1..]
+            .trim()
+            .parse::<i32>()
+            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+
         buf.clear();
+
+        rotations.push(Rotation { dir, clicks });
     }
 
-    Ok(())
+    Ok(rotations)
 }
